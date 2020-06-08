eeda755a0e1b91e289be6cc6c3860cfcd68956ec
Arturo Rendon <41806761+r2rendon@users.noreply.github.com>
2020-06-07 23:29:27 -0600

    Context added to Balance Component

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,7 +1,12 @@
 import React, { useContext } from "react";
+import { GlobalContext } from "../Context/GlobalState";
 
 export const Balance = () => {
-  const totalBal = 100;
+  const { transactions } = useContext(GlobalContext);
+  const totalAmounts = transactions.map((transaction) => transaction.amount);
+  const totalBal = totalAmounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
   return (
     <>
       <h3>Your Balance</h3>
